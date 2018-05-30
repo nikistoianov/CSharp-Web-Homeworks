76bd99ff64573d01ad315f3ab0dbf345e2ac0f33
Nikolay <nikmastera@gmail.com>
2018-05-30 17:27:28 +0300

    WebServer

---
+++ WebServer/WebServer/Launcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebServer
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WebServer/WebServer/Server/HTTP/HttpHeaderCollection.cs
@@ -0,0 +1,37 @@
+﻿namespace WebServer.Server.HTTP
+{
+    using Contracts;
+    using System.Collections.Generic;
+
+    public class HttpHeaderCollection : IHttpHeaderCollection
+    {
+        private readonly Dictionary<string, HttpHeader> headers;
+        
+        public HttpHeaderCollection()
+        {
+            headers = new Dictionary<string, HttpHeader>();
+        }
+
+        public void Add(HttpHeader header)
+        {
+            
+        }
+
+        public bool ContainsKey(string key)
+        {
+
+            return false;
+        }
+
+        public HttpHeader GetHeader(string key)
+        {
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", this.headers);
+        }
+    }
+}
