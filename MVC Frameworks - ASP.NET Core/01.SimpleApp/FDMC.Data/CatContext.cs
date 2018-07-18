faa666f2ab2983cbb2e1a8e369bd29b8dded1778
Nikolay <nikmastera@gmail.com>
2018-07-18 17:49:40 +0300

    01.SimpleApp - Initialize

---
+++ MVC Frameworks - ASP.NET Core/01.SimpleApp/FDMC.Data/CatContext.cs
@@ -0,0 +1,20 @@
+﻿using FDMC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FDMC.Data
+{
+    public class CatContext : DbContext
+    {
+        public DbSet<Cat> Cats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DbContextConfiguration.ConnectionString);
+            }
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ MVC Frameworks - ASP.NET Core/01.SimpleApp/FDMC.Data/DbContextConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace FDMC.Data
+{
+    internal static class DbContextConfiguration
+    {
+        internal static string ConnectionString => String.Format(File.ReadAllText(@"C:\Program Files\Microsoft SQL Server\MSSQL_connectionString.txt"), "Cats_MVC");
+    }
+}
