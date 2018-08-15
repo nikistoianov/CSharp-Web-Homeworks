1ecb64dc1cda22dcc20f48c65a5a16d9b15ea915
Niki-home <nikmastera@gmail.com>
2018-08-16 00:40:50 +0300

    wcr-update

---
+++ MVC Frameworks - ASP.NET Core/WCR/WCR.Web/Areas/Moderation/Pages/Add/Match.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WCR.Web.Areas.Moderation.Pages.Add
+{
+    public class MatchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
